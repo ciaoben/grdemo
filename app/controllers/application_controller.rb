13582d0df152dfdf60f3981d4ec278a8e9703f85
ciaoben <fvbenigni@gmail.com>
2024-05-16 17:24:01 +0200

    improve quality questions showed in the UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
     def current_session
         session_id = request.headers['X-Session-Id']
-        @questions = Question.order(id: :desc).limit(10)
+        @questions = Question.where(status: 2).where.not(answer: nil).order(id: :desc).limit(10)
         render json: { session_id: session_id, questions: @questions }
     end
 end
